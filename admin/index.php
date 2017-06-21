6238f5edf394d15811d6c449f417628a1434f339
WebSheep <israel@feats.com.br>
2017-06-20 21:16:03 -0300

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,56 @@
+<?php 
+	/*
+		Este arquivo é parte do Websheep CMS
+		Websheep é um software livre; você pode redistribuí-lo e/ou 
+		modificá-lo dentro dos termos da Licença Pública Geral GNU como 
+		publicada pela Fundação do Software Livre (FSF); na versão 3 da 
+		Licença, ou qualquer versão posterior.
+
+		Este programa é distribuído na esperança de que possa ser  útil, 
+		mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO
+		a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. 
+		
+		Veja a Licença Pública Geral GNU para maiores detalhes.
+		Você deve ter recebido uma cópia da Licença Pública Geral GNU junto
+		com este programa, Se não, veja <http://www.gnu.org/licenses/>.
+	*/
+	ob_start();	
+	$r = $_SERVER["DOCUMENT_ROOT"];
+	$_SERVER["DOCUMENT_ROOT"] = (substr($r, -1) == '/') ? substr($r, 0, -1) : $r;
+
+	############################################################################################################################
+	# CASO NÃO EXISTA O 'ws-config.php' IMPORTA A TELA DE SETUP
+	############################################################################################################################
+	if(!file_exists($_SERVER["DOCUMENT_ROOT"].'/ws-config.php')) {
+		include('./App/Core/ws-setup.php');
+		exit;
+	}
+
+	############################################################################################################################
+	# IMPORTAMOS A CLASSE PADRÃO DO SISTEMA
+	############################################################################################################################
+	include_once ($_SERVER["DOCUMENT_ROOT"].'/admin/App/Lib/class-ws-v1.php');
+
+	############################################################################################################################
+	#	CASO SEJA O 1° ACESSO, IMPORTA A TELA DE INSTALAÇÃO
+	############################################################################################################################
+	if(file_exists(ROOT_ADMIN.'/App/Config/firstacess') && file_get_contents(ROOT_ADMIN.'/App/Config/firstacess')=='true'){
+		include(ROOT_ADMIN.'/App/Core/ws-install.php');exit;
+	}
+
+	############################################################################################################################
+	#	CRIAMOS A 1° SESSÃO
+	############################################################################################################################
+		_session();
+	############################################################################################################################
+	#	CASO ESTEJA LOGADO IMPORTAMOS O DESKTOP
+	############################################################################################################################
+		if( SECURE==FALSE ||  (!empty($_COOKIE['ws_log']) && $_COOKIE['ws_log']=='true') && (!empty($_SESSION) && @$_SESSION['ws_log']==true)){	
+			include(ROOT_ADMIN.'/App/Core/ws-dashboard.php');exit;
+		}
+
+	############################################################################################################################
+	#	CASO ESTEJA OFFLINE JÁ DIRECIONA PRO LOGIN
+	############################################################################################################################
+	include(ROOT_ADMIN.'/App/Modulos/login/index.php');
+	exit;
